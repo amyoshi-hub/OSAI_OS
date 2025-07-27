9a0e58db59e884114e9b8b66c0e745129a6a4949
amyoshi9 <narinarit59@gmail.com>
2025-07-28 02:22:14 +0900

    add command wpa_sup and drivers

---
+++ network/target/x86_64-unknown-linux-musl/release/deps/network-9c020fb6f3466c89.d
@@ -0,0 +1,5 @@
+/home/amyoshi9/git/OSAI_OS/network/target/x86_64-unknown-linux-musl/release/deps/network-9c020fb6f3466c89: src/main.rs
+
+/home/amyoshi9/git/OSAI_OS/network/target/x86_64-unknown-linux-musl/release/deps/network-9c020fb6f3466c89.d: src/main.rs
+
+src/main.rs:
---
+++ network/target/x86_64-unknown-linux-musl/release/network.d
@@ -0,0 +1,1 @@
+/home/amyoshi9/git/OSAI_OS/network/target/x86_64-unknown-linux-musl/release/network: /home/amyoshi9/git/OSAI_OS/network/src/main.rs
