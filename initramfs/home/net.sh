6b262690ff46f2b3a720982a40af747c23033fb0
amyoshi9 <narinarit59@gmail.com>
2025-07-28 02:34:06 +0900

    success .ko so may be can luanch wifi

--- initramfs/home/net.sh
+++ initramfs/home/net.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 echo "Loading wireless modules..."
+
+insmod /lib/modules/rfkill.ko || echo "Failed to load cfg80211"
 insmod /lib/modules/cfg80211.ko || echo "Failed to load cfg80211"
 insmod /lib/modules/mac80211.ko || echo "Failed to load mac80211"
 insmod /lib/modules/iwlwifi.ko || echo "Failed to load iwlwifi"
