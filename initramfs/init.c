5b4b92ac3738aa820686ebfe30a63a4d4056c870
amyoshi9 <narinarit59@gmail.com>
2025-07-27 15:59:57 +0900

    update os command

--- initramfs/init.c
+++ initramfs/init.c
@@ -1,10 +1,28 @@
-#include <unistd.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int main() {
-    write(1, "HELLO from init\n", 16);
-    execl("/bin/sh", "sh", NULL);
-    perror("execl failed");
-    return 1;
+    printf("HELLO from init\n");
+
+    while (1) {
+        pid_t pid = fork();
+
+        if (pid == 0) {
+            char *argv[] = {"/bin/sh", NULL};
+            execve("/bin/sh", argv, NULL);
+            perror("execve failed");
+            _exit(1);
+        } else if (pid > 0) {
+            int status;
+            waitpid(pid, &status, 0);
+            printf("Shell exited, restarting...\n");
+        } else {
+            perror("fork failed");
+        }
+    }
+
+    return 0;
 }
 
